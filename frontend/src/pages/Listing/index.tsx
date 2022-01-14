ce5574de5a1c9ac008ad947a09a1357442337d7a
Jane Luz <janerodrigues_2610@hotmail.com>
2022-01-14 10:28:08 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,28 +1,36 @@
-import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination";
+import axios from 'axios';
+import MovieCard from 'components/MovieCard';
+import Pagination from 'components/Pagination';
+import { BASE_URL } from 'utils/requests';
 
-function Listing () {
+function Listing() {
+  //errada
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+    console.log(response.data)
+  });
+
   return (
-  <>
-    <Pagination/>
+    <>
+      <Pagination />
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
         </div>
       </div>
-  </>
-  );
+    </>
+  )
 }
 
-export default Listing;
+export default Listing
