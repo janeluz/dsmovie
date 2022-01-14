b36251ec66d0ae61169a36c4d93485c0702f3966
Jane Luz <janerodrigues_2610@hotmail.com>
2022-01-14 00:55:48 -0300

    _redirects

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,20 +1,21 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
-import './styles.css';
+import './styles.css'
 function Navbar() {
   return (
     <header>
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/janeluz"></a>
-          <div className="dsmovie-contact-container">
-            <GithubIcon />
-            <p className="dsmovie-contact-link">/devsuperior</p>
-          </div>
+          <a href="https://github.com/janeluz" target="_blank" rel="noreferrer">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
         </div>
       </nav>
     </header>
-  );
+  )
 }
 
 export default Navbar
